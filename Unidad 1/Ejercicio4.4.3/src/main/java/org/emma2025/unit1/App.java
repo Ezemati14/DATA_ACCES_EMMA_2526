bb876e18d6e78376278974386b236847e2d316b0
EzeMati14 <ezematitrabajo@gmail.com>
2025-10-20 17:55:19 +0200

    Ejercicio 4.4.3

---
+++ Unidad 1/Ejercicio4.4.3/src/main/java/org/emma2025/unit1/App.java
@@ -0,0 +1,13 @@
+package org.emma2025.unit1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
